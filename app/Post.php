07dfa8dc51da007568850469c10bc34cb676d895
Weslei Ferreira <desenvolvimento2@interativasistemas.com>
2017-10-18 11:48:08 -0200

    2017-10-18 1.1

--- app/Post.php
+++ app/Post.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -10,7 +12,39 @@ class Post extends Model
         'type', 'title', 'body'
     ];
 
-    protected $dates = ['created_at', 'updated_at', 'published_at'];
+    protected $dates = ['created_at', 'updated_at', 'published_at'];//Especificando estes campos para utilizar o Carbor (Data, pacote de data para trabalhar no PHP)
+
+    //Configuro um evento padrão onde setamos o atributo para ficar lower case. O nome da função deve seguir este padrão setNomedaPropriedadeAttribute ou get para retornar na tela para o usuário
+    public function setTitleAttribute($value){
+        $this->attributes['title'] = strtolower($value);
+    }
+
+    public function getTitleAttribute($value){
+        return ucfirst($value);
+    }
+
+    //Scopos - o nome da função deve começar com scope
+    public function scopeOfType($query, $type){
+        return $query->where('type', $type);
+    }
+
+    public function scopeText(Builder $query){
+        return $query->where('type', 'text');
+    }
+
+    //Método de inicialização
+    protected static function boot(){
+        parent::boot();
+        //Adicionando um escopo global
+        static::addGlobalScope('published', function(Builder $builder){
+            $builder->where('published_at', '<', Carbon::now()->format('y-m-d H:i:s'));
+        });
+    }
+
+    //Configurando um cast automático diretamente no model
+//    protected $casts = [
+//        'is_true'=> 'boolean'
+//    ];
 
     //protected $guard  = []; Utilizar o fillable ou guard para especificar quais capos podem ser alterados
 
